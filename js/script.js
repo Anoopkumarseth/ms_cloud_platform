8f5c1f9ee4c9a2f5a0d03ff2295a8967ffa4dac8
Anoop Kumar Seth <anoopkumarseth@hotmail.com>
2022-06-05 16:51:04 +0530

    tab and scroll top

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
+// custom tab
 tablinks = document.querySelectorAll(".tab .tab-link");
 tabContents = document.querySelectorAll(".tab-content");
-
-for (const item of tablinks) {
-    item.addEventListener("click", function(e){
+for (let i = 0;i<tablinks.length;i++) {
+    tablinks[i].addEventListener("click", (e) =>{
         e.preventDefault()
-        console.log(this)
-        tablinks.forEach((el) => el.classList.remove('active'));
-        this.classList.add("active");
+        tablinks.forEach((item) => item.classList.remove('active'));
+        tablinks[i].classList.add("active");
+        tabContents.forEach((item) => item.classList.remove('active'));
+        tabContents[i].classList.add("active");
     })
 }
+
+// scroll to top
+const scrollTop = document.querySelector(".scrollTop");
+const scrollAppearance = () => {
+    if (window.scrollY > 320) {
+        scrollTop.classList.add("active");
+    } else {
+        scrollTop.classList.remove("active");
+    }
+};
+document.addEventListener("scroll", () => {
+    scrollAppearance();
+});
+scrollTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
